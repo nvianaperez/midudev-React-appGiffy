f2900460772a37ac3b63792e06f11995940ed1be
nviana <nvianaperez@gmail.com>
2022-02-24 13:11:36 +0100

    wouter added - error ListOfGifts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
 import ListOfGifs from "./Components/ListOfGifs";
+//utilizamos wouter en vez de router.
+//wouter es un paquete que pesa bastante menos que router y no tiene dependencias
+//npm install wouter (porque y inicié el proyecto con npm)--> si hemos creado la app con yarn seguir con yarn porque sino npm instalaría toooodas las dependencias de cero otra vez.
+import { Link, Route } from "wouter";
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword="panda" />
+        <h1>App</h1>
+        <Link to="gif/cats">Gifs de gatos</Link>
+        <Link to="gif/perros">Gifs de perros</Link>
+        <Link to="gif/pandas">Gifs de pandas</Link>
+        <Route path="/gif/:keyword" component={ListOfGifs} />
       </section>
     </div>
   );
--- src/Components/ListOfGifs.js
+++ src/Components/ListOfGifs.js
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from "react";
-import Gif from "./Gif";
+import Gif from "../Components/Gif";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ keyword }) {
+export default function ListOfGifs({ params }) {
+  //dentro del objeto params me guarda las keywords
+  const { keyword } = params;
+
   const [gifs, setGifs] = useState([]);
 
   // cambiar el gif cada vez que se actualice el estado y se renderiza el componente
-  useEffect(function () {
-    //en getGifs se hace el fetch
-    getGifs({ keyword }).then((gifs) => setGifs(gifs));
-  }, [keyword]);
+  useEffect(
+    function () {
+      //en getGifs se hace el fetch
+      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    },
+    [keyword]
+  );
   //cuidado con el loop infinito, hay que entrar las dependiencias de este efecto
   //si no tiene dependencias el array viene vacío y EL EFECTO SE PRODUCE SOLO UNA VEZ!
   //esta keyword es una dependencia del proyecto
   //porque cada vez que se cambie la prop que le llega (keyword) se vuelva a llamar al efecto y se vuelve a renderizar
 
-  return gifs.map(({ title, url, id }) => (
-    <Gif key={id} id={id} title={title} url={url} />
-  ));
+  return (
+    <div>
+      gifs.map(({(title, id, url)}) =>
+      <Gif key={id} id={id} title={title} url={url} />)
+    </div>
+  );
 }
 
 /* {gifs.map((singleGif) => (
