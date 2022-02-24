ab186889f04333df25f9742c8bbb7fda0491e8b9
nviana <nvianaperez@gmail.com>
2022-02-24 12:05:12 +0100

    giffy app without routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import getGifs from "./services/getGifs";
 import ListOfGifs from "./Components/ListOfGifs";
 
 function App() {
-  const [gifs, setGifs] = useState([]);
-
-  // cambiar el gif cada vez que se actualice el estado y se renderiza el componente
-  useEffect(function () {
-    //en getGifs se hace el fetch
-    getGifs({ keyword: "cats" }).then((gifs) => setGifs(gifs));
-  }, []);
-  //cuidado con el loop infinito, hay que entrar las dependiencias de este efecto
-  //si no tiene dependencias el array viene vacío y el efecto se produce solo una vez
-
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs gifs={gifs} />
+        <ListOfGifs keyword="panda" />
       </section>
     </div>
   );
--- src/Components/ListOfGifs.js
+++ src/Components/ListOfGifs.js
@@ -1,7 +1,20 @@
-import React from "react";
-import Gif from "./Components/Gif";
+import React, { useState, useEffect } from "react";
+import Gif from "./Gif";
+import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ gifs }) {
+export default function ListOfGifs({ keyword }) {
+  const [gifs, setGifs] = useState([]);
+
+  // cambiar el gif cada vez que se actualice el estado y se renderiza el componente
+  useEffect(function () {
+    //en getGifs se hace el fetch
+    getGifs({ keyword }).then((gifs) => setGifs(gifs));
+  }, [keyword]);
+  //cuidado con el loop infinito, hay que entrar las dependiencias de este efecto
+  //si no tiene dependencias el array viene vacío y EL EFECTO SE PRODUCE SOLO UNA VEZ!
+  //esta keyword es una dependencia del proyecto
+  //porque cada vez que se cambie la prop que le llega (keyword) se vuelva a llamar al efecto y se vuelve a renderizar
+
   return gifs.map(({ title, url, id }) => (
     <Gif key={id} id={id} title={title} url={url} />
   ));
